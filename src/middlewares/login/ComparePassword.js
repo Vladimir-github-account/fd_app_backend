f6d5b7fe2139ddd1d4c957ffde90fc67176f1401
vladimir <s>
2020-02-14 22:25:59 +0200

    Refactor: login MW file splitting

--- src/middlewares/login/ComparePassword.js
+++ src/middlewares/login/ComparePassword.js
@@ -1,25 +1,16 @@
 import bcrypt from 'bcrypt';
 import AppError from '../../utils/application_errors';
-import { User } from '../../db/models';
 
 export default async function (req, res, next){
     try {
-        const { email } = req.body;
-        const user = await User.findOne({
-            where:{
-                email
-            }
-        });
-        if (user){
-            const { password: requestPassword } = req.body;
-            if( await bcrypt.compare(requestPassword, user.password) ){
-                const userData = user.get();
-                delete userData.password;
-                return res.status(200).send(userData);
-            }
-            next( new AppError.InvalidPasswordError() );
+        const { user } = req;
+        const { password: requestPassword } = req.body;
+        if( await bcrypt.compare(requestPassword, user.password) ){
+            const userData = user.get();
+            delete userData.password;
+            return res.status(200).send(userData);
         }
-        next(new AppError.NotFoundError('user'));
+        next( new AppError.InvalidPasswordError() );
     }catch (e) {
         next(e);
     }
