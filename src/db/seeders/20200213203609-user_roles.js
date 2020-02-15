f70dd50f9bb691f8b1813627df60ece6942f172a
vladimir <s>
2020-02-15 17:42:18 +0200

    Add: correct user roles seed

--- src/db/seeders/20200213203609-user_roles.js
+++ src/db/seeders/20200213203609-user_roles.js
@@ -1,39 +1,37 @@
 'use strict';
 
+function generateUserRoles (users){
+  const userRoles = [];
+  users.forEach( (user, index) => {
+    const userRolesCount = ( Math.round(Math.random() * 10) % 3 ) + 1;
+    for (let j = 0; j < userRolesCount; j++) {
+      if (j === 0) {
+        userRoles.push( {
+          userId: users[index].id,
+          roleId: j + 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        } );
+      } else if ( Math.round((Math.random() * 10) % 2) === 0){
+        userRoles.push( {
+          userId: users[index].id,
+          roleId: j + 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        } );
+      }
+    }
+  });
+  return userRoles;
+}
+
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('UserRoles', [
-      {
-        userId: 1,
-        roleId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 1,
-        roleId: 2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 1,
-        roleId: 3,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 2,
-        roleId: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userId: 3,
-        roleId: 2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ], {});
+    return queryInterface.sequelize.query( `SELECT "Users".id FROM "Users"` )
+        .then( data => {
+          return queryInterface.bulkInsert('UserRoles', generateUserRoles(data[0]), {});
+        })
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('UserRoles', null, {});
